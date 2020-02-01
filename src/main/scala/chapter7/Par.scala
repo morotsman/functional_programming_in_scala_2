fcb6bd46d58fc5727321c9079a18f298563e6f81
Niklas Leopold <leopold.niklas@gmail.com>
2020-02-01 09:42:15 +0100

    more

--- src/main/scala/chapter7/Par.scala
+++ src/main/scala/chapter7/Par.scala
@@ -3,7 +3,9 @@ package chapter7
 trait Par[+A] {}
 
 object Par {
-  def unit[A](a: => A): Par[A] = ???
-  def get[A](pa: Par[A]): A = ???
-  def map2[A, B, C](pa: => Par[A], pb: => Par[B])(f: (A, B) => C): Par[C] = ???
+  def unit[A](a: A): Par[A] = ???
+  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] = ???
+  def fork[A](a: => Par[A]): Par[A] = ???
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def run[A](pa: Par[A]): A = ???
 }
