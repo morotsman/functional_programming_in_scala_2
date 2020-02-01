8454e2af13e8b5f7ea0e95f2822197e48268ff19
Niklas Leopold <leopold.niklas@gmail.com>
2020-02-01 11:50:34 +0100

    more

--- src/main/scala/chapter7/Par.scala
+++ src/main/scala/chapter7/Par.scala
@@ -2,6 +2,8 @@ package chapter7
 
 import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
+import chapter3.{List, Cons}
+
 
 object Par {
   type Par[A] = ExecutorService => Future[A]
@@ -19,6 +21,9 @@ object Par {
   def unit[A](a: A): Par[A] =
     es => UnitFuture(a)
 
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] =
+    map2(pa, unit(()))((a, _) => f(a))
+
   def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
     es => {
       val af = pa(es)
@@ -30,6 +35,17 @@ object Par {
     es => es.submit(() => a(es).get())
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    ps.foldRight(unit(List[A]()))((pa, pla) => map2(pa, pla)((a, la) => Cons(a, la)))
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val lpb: List[Par[B]] = ps.map(asyncF(f))
+    sequence(lpb)
+  }
 
   def run[A](s: ExecutorService)(pa: Par[A]): Future[A] = pa(s)
 }
--- src/test/scala/chapter7/ParTest.scala
+++ src/test/scala/chapter7/ParTest.scala
@@ -4,25 +4,42 @@ import java.util.concurrent.Executors
 
 import org.scalatest.FunSuite
 import Par._
+import chapter3.List
+import chapter8.{Gen, Prop}
 
 class ParTest extends FunSuite {
+
+  val es = Executors.newFixedThreadPool(100)
 
 
   test("Par usage") {
     def sum(ints: IndexedSeq[Int]): Par[Int] = {
-      println("sum: " + ints)
-      Thread.sleep(1000)
       if (ints.size <= 1)
         Par.unit(ints.headOption getOrElse (0))
       else {
-        val (l, r) =  ints.splitAt(ints.length / 2)
-        Par.map2(fork(sum(l)), fork(sum(r)))(_+_)
+        val (l, r) = ints.splitAt(ints.length / 2)
+        Par.map2(fork(sum(l)), fork(sum(r)))(_ + _)
       }
     }
 
-
-    val es = Executors.newFixedThreadPool(100)
-    assert(Par.run(es)(sum(List(1,2,3,4,5,6,7,8,9).toIndexedSeq)).get() == 45)
+    val prop = Prop.forAll(Gen.listOf(Gen.int)) { l =>
+      Par.run(es)(sum(l.toScalaList().toIndexedSeq)).get() == l.toScalaList().sum
+    }
+    Prop.run(prop)
   }
 
+  test("asyncF") {
+    def add2(a: Int): Int = a + 2
+
+    val asyncAdd2: Int => Par[Int] = asyncF(add2)
+    assert(Par.run(es)(asyncAdd2(2)).get() == 4)
+  }
+
+  test("sequence") {
+    val prop = Prop.forAll(Gen.listOf(Gen.int)) { l =>
+      val seq = sequence(l.map(unit))
+      Par.run(es)(seq).get() == l
+    }
+    Prop.run(prop)
+  }
 }
