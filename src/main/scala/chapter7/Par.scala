387ad3a2d24baac727ccaeeca3aec813ce380dbb
Niklas Leopold <leopold.niklas@gmail.com>
2020-02-01 10:20:55 +0100

    more

--- src/main/scala/chapter7/Par.scala
+++ src/main/scala/chapter7/Par.scala
@@ -1,11 +1,36 @@
 package chapter7
 
-trait Par[+A] {}
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
 
 object Par {
-  def unit[A](a: A): Par[A] = ???
-  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] = ???
-  def fork[A](a: => Par[A]): Par[A] = ???
+  type Par[A] = ExecutorService => Future[A]
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+
+    override def isCancelled: Boolean = false
+
+    override def isDone: Boolean = true
+
+    override def get(timeout: Long, unit: TimeUnit): A = get
+  }
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = pa(es)
+      val bf = pb(es)
+      UnitFuture(f(af.get(), bf.get()))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      override def call(): A = a(es).get()
+    })
+
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
-  def run[A](pa: Par[A]): A = ???
+
+  def run[A](s: ExecutorService)(pa: Par[A]): Future[A] = pa(s)
 }
--- src/test/scala/chapter7/ParTest.scala
+++ src/test/scala/chapter7/ParTest.scala
@@ -1,8 +1,12 @@
 package chapter7
 
+import java.util.concurrent.Executors
+
 import org.scalatest.FunSuite
+import Par._
 
 class ParTest extends FunSuite {
+
 
   test("Par usage") {
     def sum(ints: IndexedSeq[Int]): Par[Int] =
@@ -12,6 +16,8 @@ class ParTest extends FunSuite {
         val (l, r) =  ints.splitAt(ints.length / 2)
         Par.map2(sum(l), sum(r))(_+_)
       }
+    val es = Executors.newFixedThreadPool(2)
+    assert(Par.run(es)(sum(List(1,2,3).toIndexedSeq)).get() == 6)
   }
 
 }
