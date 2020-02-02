81fae14238a29344b704e9c51d29ed4a827f4fe0
Niklas Leopold <leopold.niklas@gmail.com>
2020-02-02 15:58:45 +0100

    more

---
+++ src/main/scala/chapter13/Factorial5.scala
@@ -0,0 +1,62 @@
+package chapter13
+
+import chapter13.Free._
+
+object Factorial5 {
+
+  val helpstring =
+    """
+      | The Amazing Factorial REPL, v2.0
+      | q - quit
+      | <number> - compute the factorial of the given number
+      | <anything else> - bomb with horrible error
+  """.trim.stripMargin
+
+
+  def factorial(n: Int): Int = {
+    def go(i: Int, acc: Int): Int =
+      if (i > n) acc
+      else if (i == 0) go(i + 1, 1)
+      else go(i + 1, acc * i)
+
+    go(0, 1)
+  }
+
+  def PrintLine(s: String): TailRecType[Unit] =
+    Suspend(() => {
+      Return(println(s))
+    })
+
+  def ReadLine: TailRecType[String] =
+    Suspend(() => {
+      readLine
+    })
+
+
+  def factorialREPL2: TailRecType[Unit] = TailRec.sequence_(
+    PrintLine(helpstring),
+    TailRec.doWhile { ReadLine } { line =>
+      val ok = line != "q"
+      TailRec.when(ok) {
+        for {
+          - <- PrintLine("factorial: "  + factorial(line.toInt).toString)
+        } yield ()
+      }
+    })
+
+  def factorialREPL3: TailRecType[Unit] =for {
+    _ <- PrintLine(helpstring)
+    _ <- TailRec.doWhile { ReadLine } { line =>
+      val ok = line != "q"
+      TailRec.when(ok) {
+        for {
+          - <- PrintLine("factorial: "  + factorial(line.toInt).toString)
+        } yield ()
+      }
+    }
+  } yield ()
+
+  def main(args: Array[String]): Unit = {
+    runTrampoline(factorialREPL2)
+  }
+}
--- src/main/scala/chapter13/Free.scala
+++ src/main/scala/chapter13/Free.scala
@@ -21,9 +21,43 @@ object Free {
   case class FlatMap[F[_], A, B](s: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
 
   def freeMonad[F[_]]: Monad2[({type f[a] = Free[F, a]})#f] = new Monad2[({type f[a] = Free[F, a]})#f] {
-    override def flatMap[A, B](fa: Free[F, A])(f: A => Free[F, B]): Free[F, B] = ???
+    override def flatMap[A, B](fa: Free[F, A])(f: A => Free[F, B]): Free[F, B] =
+      fa flatMap f
 
-    override def unit[A](a: => A): Free[F, A] = ???
+    override def unit[A](a: => A): Free[F, A] = Return(a)
   }
+
+  val TailRec = freeMonad[Function0]
+
+  type TailRecType[A] = Free[Function0, A]
+
+  @annotation.tailrec
+  def runTrampoline[A](a: Free[Function0, A]): A = a match {
+    case Return(a) => a
+    case Suspend(r) => r()
+    case FlatMap(x, f) => x match {
+      case Return(a) => runTrampoline {
+        f(a)
+      }
+      case Suspend(r) => runTrampoline {
+        f(r())
+      }
+      case FlatMap(y, g) => runTrampoline(y.flatMap(a => g(a).flatMap(f)))
+    }
+  }
+
+  //@annotation.tailrec
+  def step[F[_], A](free: Free[F, A])(implicit F: Monad2[F]): Free[F, A] = free match {
+    case FlatMap(FlatMap(x, f), g) => step(x.flatMap(a => f(a).flatMap(g)))
+    case FlatMap(Return(x), f) => step(f(x))
+    case _ => free
+  }
+
+  def run[F[_], A](free: Free[F, A])(implicit F: Monad2[F]): F[A] = step(free) match {
+    case Return(a) => F.unit(a)
+    case Suspend(r) => r
+    case _ => sys.error("Not possible since step has already matched")
+  }
+
 
 }
