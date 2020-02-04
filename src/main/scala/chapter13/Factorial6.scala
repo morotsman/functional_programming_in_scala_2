4f1d3075d5ab303c94b630a2beeb58e87137b510
Niklas Leopold <leopold.niklas@gmail.com>
2020-02-04 18:58:55 +0100

    more

---
+++ src/main/scala/chapter13/Factorial6.scala
@@ -0,0 +1,52 @@
+package chapter13
+
+import chapter13.Free._
+import chapter13.Free.Console._
+
+object Factorial6 {
+
+  val helpstring =
+    """
+      | The Amazing Factorial REPL, v2.0
+      | q - quit
+      | <number> - compute the factorial of the given number
+      | <anything else> - bomb with horrible error
+  """.trim.stripMargin
+
+
+  def factorial(n: Int): Int = {
+    def go(i: Int, acc: Int): Int =
+      if (i > n) acc
+      else if (i == 0) go(i + 1, 1)
+      else go(i + 1, acc * i)
+
+    go(0, 1)
+  }
+
+  def PrintLine(s: String): TailRecType[Unit] =
+    Suspend(() => {
+      Return(println(s))
+    })
+
+  def ReadLine: TailRecType[String] =
+    Suspend(() => {
+      readLine
+    })
+
+
+  def factorialREPL: Free[Console, Unit] =for {
+    _ <- printLn(helpstring)
+    _ <- freeMonad.doWhile { readLn } { line =>
+      val ok = line.get != "q"
+      freeMonad.when(ok) {
+        for {
+          - <- printLn("factorial: "  + factorial(line.get.toInt).toString)
+        } yield ()
+      }
+    }
+  } yield ()
+
+  def main(args: Array[String]): Unit = {
+    runConsole(factorialREPL)
+  }
+}
