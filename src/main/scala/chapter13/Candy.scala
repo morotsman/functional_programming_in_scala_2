9540d579411a5803014c1aded8921efde7a1c657
Niklas Leopold <leopold.niklas@gmail.com>
2020-02-09 11:58:04 +0100

    more

--- src/main/scala/chapter13/Candy.scala
+++ src/main/scala/chapter13/Candy.scala
@@ -15,6 +15,49 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object Candy {
 
+  def main(args: Array[String]): Unit = {
+    //candyMachine(Machine(true, 10, 0))
+
+    val res = runConsoleState(candyProgram(Machine(true, 10, 0))).run(Buffers(List("c"), List()))
+    res._2.out.reverse().forEach(a => println(a))
+    println(res._1)
+
+    val res2 = runConsoleState(candyProgram(Machine(false, 10, 1))).run(Buffers(List("c"), List()))
+    res2._2.out.reverse().forEach(a => println(a))
+    println(res2._1)
+
+    val res3 = runConsoleState(candyProgram(Machine(false, 10, 1))).run(Buffers(List("t"), List()))
+    res3._2.out.reverse().forEach(a => println(a))
+    println(res3._1)
+  }
+
+  def candyMachine(machine: Machine): Unit = {
+      runConsole(candyProgram(machine)) match {
+      case Left(_) =>
+        candyMachine(machine)
+      case Right(newMachine) =>
+        candyMachine(newMachine)
+    }
+  }
+
+  def candyProgram(machine: Machine): Free[Console, Either[String, Machine]] = for {
+    _ <- printLn("")
+    _ <- showCurrentStatusProgram(machine)
+    - <- printLn("Input: c(coin) or t(turn)")
+    input <- readLn
+    eitherMachine <- freeMonad.unit(rule(machine, input.map(i => if (i == "c") Coin else Turn).get))
+    - <- eitherMachine match {
+      case Left (message)  => printLn("Error: " + message)
+      case _ => printLn("")
+    }
+  } yield (eitherMachine)
+
+  def showCurrentStatusProgram(m: Machine): Free[Console, Unit] = m match {
+    case Machine(locked, candies, coins) if locked => printLn(s"The machine is locked and has $candies candies left")
+    case Machine(locked, candies, coins) => printLn(s"The machine is unlocked and has $candies candies left")
+  }
+
+
   private def rule(machine: Machine, input: Input): Either[String, Machine] = input match {
     case Coin =>
       if (machine.candies == 0) {
@@ -34,53 +77,5 @@ object Candy {
       } else {
         Left("You need to dispose a coin to get a candy")
       }
-  }
-
-  def candyProgram(m: Machine): Free[Console, Either[String, Machine]] =
-    for {
-      - <- printLn("Input: c(coin) or t(turn)")
-      input <- readLn
-    } yield (
-      rule(m, input.map(i => if (i == "c") Coin else Turn).get)
-    )
-
-  def errorInfoProgram(message: String): Free[Console, Unit] =
-    printLn("Error: " + message)
-
-  def showCurrentStatusProgram(m: Machine): Free[Console, Unit] = m match {
-    case Machine(locked, candies, coins) if locked => printLn(s"The machine is locked and has $candies candies left")
-    case Machine(locked, candies, coins) => printLn(s"The machine is unlocked and has $candies candies left")
-  }
-
-  def input(machine: Machine): Free[Console, Either[String, Machine]] = for {
-    _ <- printLn("")
-    _ <- showCurrentStatusProgram(machine)
-    result <- candyProgram(machine)
-  } yield result
-
-  def candyMachine(machine: Machine): Unit = {
-      runConsole(input(machine)) match {
-      case Left(message) =>
-        runConsole(errorInfoProgram("Error: " + message))
-        candyMachine(machine)
-      case Right(m) =>
-        candyMachine(m)
-    }
-  }
-
-  def main(args: Array[String]): Unit = {
-    candyMachine(Machine(true, 10, 0))
-
-    val res = runConsoleState(input(Machine(true, 10, 0))).run(Buffers(List("c"), List()))
-    res._2.out.reverse().forEach(a => println(a))
-    println(res._1)
-
-    val res2 = runConsoleState(input(Machine(false, 10, 1))).run(Buffers(List("c"), List()))
-    res2._2.out.reverse().forEach(a => println(a))
-    println(res2._1)
-
-    val res3 = runConsoleState(input(Machine(false, 10, 1))).run(Buffers(List("t"), List()))
-    res3._2.out.reverse().forEach(a => println(a))
-    println(res3._1)
   }
 }
