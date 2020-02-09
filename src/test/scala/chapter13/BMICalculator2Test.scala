4818ef0fe7921b644da80a7bee92a1003a5ef412
Niklas Leopold <leopold.niklas@gmail.com>
2020-02-09 17:01:21 +0100

    more

--- src/test/scala/chapter13/BMICalculator2Test.scala
+++ src/test/scala/chapter13/BMICalculator2Test.scala
@@ -1,5 +1,7 @@
 package chapter13
 
+import chapter13.Free._
+import chapter3.List
 import org.scalatest.FunSuite
 
 class BMICalculator2Test extends FunSuite {
@@ -8,8 +10,36 @@ class BMICalculator2Test extends FunSuite {
 
   }
 
-  test("testBmiProgram") {
+  test("single run") {
+    val res: (Unit, Buffers) = runConsoleState(BMICalculator2.bmiProgram()).run(Buffers(List("", "70", "178", "q"), List()))
+    assert(res._2.out.reverse() == List(
+      BMICalculator2.helpstring,
+      "q to quit, any other key continue...",
+      "Please enter your weight: ",
+      "Please enter your height (cm): ",
+      "Your have normal weight and your bmi is: 22.093170054286073",
+      "q to quit, any other key continue..."
+    ))
+  }
 
+  test("multiple runs") {
+    val res: (Unit, Buffers) = runConsoleState(BMICalculator2.bmiProgram()).run(Buffers(List("", "70", "178", "", "90", "178", "", "50", "178", "q"), List()))
+    assert(res._2.out.reverse() == List(
+      BMICalculator2.helpstring,
+      "q to quit, any other key continue...",
+      "Please enter your weight: ",
+      "Please enter your height (cm): ",
+      "Your have normal weight and your bmi is: 22.093170054286073",
+      "q to quit, any other key continue...",
+      "Please enter your weight: ",
+      "Please enter your height (cm): ",
+      "Your overweight, your bmi is: 28.40550435551067",
+      "q to quit, any other key continue...",
+      "Please enter your weight: ",
+      "Please enter your height (cm): ",
+      "Sorry, your underweight, your bmi is: 15.780835753061481",
+      "q to quit, any other key continue..."
+    ))
   }
 
   test("testBmiPrompt") {
@@ -27,7 +57,4 @@ class BMICalculator2Test extends FunSuite {
   test("testHelpstring") {
 
   }
-
-  //val res: (Unit, Buffers) = runConsoleState(program).run(Buffers(List("", "70", "178", "q"), List()))
-  //res._2.out.reverse().forEach(a => println(a))
 }
